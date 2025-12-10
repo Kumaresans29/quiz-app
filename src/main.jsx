c338d9d921be8c7b25008446c2b0587343499577
skumaresan032003-svg <your_github_email@example.com>
2025-12-10 13:26:55 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { HashRouter as Router } from "react-router-dom";
 
-
 createRoot(document.getElementById('root')).render(
   <Router>
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+      <App />
+    </StrictMode>
   </Router>
 )
